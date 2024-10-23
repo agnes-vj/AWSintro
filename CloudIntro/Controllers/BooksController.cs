b9142e92e58218308118aab099b3fce219bf9ee0
Richard Neat <richard.neat@northcoders.com>
2024-10-23 14:04:22 +0100

    add starter code

---
+++ CloudIntro/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using CloudIntro.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudIntro.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly IBookService _bookService;
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+        public IActionResult Index()
+        {
+            return Ok(_bookService.FindBooks());
+        }
+    }
+}
